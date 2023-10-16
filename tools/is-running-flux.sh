d873968a533f69f66a1272404ef3736ba683b393
Laszlo Uveges <laszlo@giantswarm.io>
2023-10-16 12:11:44 +0200

    Add small script to check for expected controller versions before and after upgrade

---
+++ tools/is-running-flux.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+VERSION=$1; shift
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [[ "$VERSION" == "v1" ]]; then
+  echo "Checking for version: ${VERSION}"
+
+  EXPECTED_HELM_CONTROLLER_VERSION="v0.31.2"
+  EXPECTED_IMAGE_AUTOMATION_CONTROLLER_VERSION="v0.31.0"
+  EXPECTED_IMAGE_REFLECTOR_CONTROLLER_VERSION="v0.26.1"
+  EXPECTED_KUSTOMIZE_CONTROLLER_VERSION="v0.35.1"
+  EXPECTED_NOTIFICATION_CONTROLLER_VERSION="v0.33.0"
+  EXPECTED_SOURCE_CONTROLLER_VERSION="v0.36.1"
+elif [[ "$VERSION" == "v2" ]]; then
+  echo "Checking for version: ${VERSION}"
+
+  EXPECTED_HELM_CONTROLLER_VERSION="v0.35.0"
+  EXPECTED_IMAGE_AUTOMATION_CONTROLLER_VERSION="v0.35.0"
+  EXPECTED_IMAGE_REFLECTOR_CONTROLLER_VERSION="v0.29.1"
+  EXPECTED_KUSTOMIZE_CONTROLLER_VERSION="v1.0.1"
+  EXPECTED_NOTIFICATION_CONTROLLER_VERSION="v1.0.0"
+  EXPECTED_SOURCE_CONTROLLER_VERSION="v1.0.1"
+else
+  echo "Unknown version! Either pass 'v1' or 'v2'!"
+  exit 1
+fi
+
+function check_controller_version() {
+  namespace="$1"; shift
+  label="$1"; shift
+  expected_version="$1"; shift
+
+  echo "## Checking for pod in '${namespace}' with label '${label}'..."
+
+  actual_image="$(kubectl -n "${namespace}" get pod -l "${label}" -o yaml | yq '.items[].spec.containers[].image')"
+  actual_tag="$(echo "$actual_image" | cut -d ":" -f 2)"
+
+  if [[ "${actual_tag}" == "${expected_version}" ]]; then
+    echo -e "  > ${GREEN}Passed!${NC} (Expected: '${expected_version}' and got: '${actual_tag}')"
+  else
+    echo -e "  > ${RED}Failed!${NC} Pod in '${namespace}' with label '${label}' does not match expected version: '${expected_version}', but was: '${actual_tag}'!"
+  fi
+}
+
+echo ""
+echo "# Checking: 'flux-giantswarm' pods..."
+echo ""
+
+check_controller_version "flux-giantswarm" "app=helm-controller" "${EXPECTED_HELM_CONTROLLER_VERSION}"
+check_controller_version "flux-giantswarm" "app=image-automation-controller" "${EXPECTED_IMAGE_AUTOMATION_CONTROLLER_VERSION}"
+check_controller_version "flux-giantswarm" "app=image-reflector-controller" "${EXPECTED_IMAGE_REFLECTOR_CONTROLLER_VERSION}"
+check_controller_version "flux-giantswarm" "app=kustomize-controller" "${EXPECTED_KUSTOMIZE_CONTROLLER_VERSION}"
+check_controller_version "flux-giantswarm" "app=notification-controller" "${EXPECTED_NOTIFICATION_CONTROLLER_VERSION}"
+check_controller_version "flux-giantswarm" "app=source-controller" "${EXPECTED_SOURCE_CONTROLLER_VERSION}"
+
+echo ""
+echo "# Checking: 'flux-system' pods..."
+echo ""
+
+check_controller_version "flux-system" "app=helm-controller" "${EXPECTED_HELM_CONTROLLER_VERSION}"
+check_controller_version "flux-system" "app=image-automation-controller" "${EXPECTED_IMAGE_AUTOMATION_CONTROLLER_VERSION}"
+check_controller_version "flux-system" "app=image-reflector-controller" "${EXPECTED_IMAGE_REFLECTOR_CONTROLLER_VERSION}"
+check_controller_version "flux-system" "app=kustomize-controller" "${EXPECTED_KUSTOMIZE_CONTROLLER_VERSION}"
+check_controller_version "flux-system" "app=notification-controller" "${EXPECTED_NOTIFICATION_CONTROLLER_VERSION}"
+check_controller_version "flux-system" "app=source-controller" "${EXPECTED_SOURCE_CONTROLLER_VERSION}"
