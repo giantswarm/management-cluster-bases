9c9b33d73b11a6301cafa9342a6eb4645c586649
Lukasz Jakimczuk <39192420+ljakimczuk@users.noreply.github.com>
2023-10-17 15:47:25 +0200

    Test konfigure (#45)
    ## Some hints on changes to this repository
    
    ### Public information only
    
    This is a public repository. The content and commit history is visible
    to everyone.
    
    Do not provide any **customer-specific details**. Use the customer's
    repository for that.
    
    ### Descriptive PR title
    
    Please write a descriptive pull request title. In this repo we don't
    maintain a changelog file, so the PR title (becoming the commit message
    after squash-merge) is the main information to understand a change in
    retrospect.

--- bases/tools/Makefile.custom.mk
+++ bases/tools/Makefile.custom.mk
@@ -99,9 +99,9 @@ endif
 $(BUILD_MC_TARGETS): $(KUSTOMIZE) $(HELM) $(YQ)
 	@echo "====> $@"
 	mkdir -p output
-	$(KUSTOMIZE) build --load-restrictor LoadRestrictionsNone --enable-helm --helm-command="$(HELM)" management-clusters/$(subst build-,,$@) > output/$(subst build-,,$@).prep.yaml
+	$(KUSTOMIZE) build --enable-alpha-plugins --load-restrictor LoadRestrictionsNone --enable-helm --helm-command="$(HELM)" management-clusters/$(subst build-,,$@) > output/$(subst build-,,$@).prep.yaml
 	echo '---' >> output/$(subst build-,,$@).prep.yaml
-	$(KUSTOMIZE) build --load-restrictor LoadRestrictionsNone --enable-helm --helm-command="$(HELM)" management-clusters/$(subst build-,,$@)/extras >> output/$(subst build-,,$@).prep.yaml
+	$(KUSTOMIZE) build --enable-alpha-plugins --load-restrictor LoadRestrictionsNone --enable-helm --helm-command="$(HELM)" management-clusters/$(subst build-,,$@)/extras >> output/$(subst build-,,$@).prep.yaml
 	# extract variables from the `flux` Kustomization CR
 	$(YQ) e 'select(.kind == "Kustomization") | select(.metadata.name == "flux") | .spec.postBuild.substitute.[] | "export " + key + "=" + @sh' output/$(subst build-,,$@).prep.yaml > output/$(subst build-,,$@).env
 	# extract variables as scope for `envsubst` to not risk replacing too much
