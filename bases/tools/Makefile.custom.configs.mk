0c4c9d2d5d26fd01c99b55d2c02ec981376e439f
Laszlo Uveges <laszlo@giantswarm.io>
2023-11-28 16:23:29 +0100

    Get back to HTTPS clone for shared-configs

--- bases/tools/Makefile.custom.configs.mk
+++ bases/tools/Makefile.custom.configs.mk
@@ -1,7 +1,7 @@
 TMPDIR := $(shell mktemp -d)
 
 assemble-config:
-	@git clone --quiet git@github.com:giantswarm/shared-configs.git $$TMPDIR
+	@git clone --quiet https://github.com/giantswarm/shared-configs $$TMPDIR
 	@mv $$TMPDIR/default ./
 	@mv $$TMPDIR/include ./
 	@rm -rf $$TMPDIR
