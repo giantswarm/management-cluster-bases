47335ca1b4c8cac3b1fa6d66a1e361a116b628bb
Spyros Synodinos <spyros@giantswarm.io>
2024-03-07 11:45:57 +0200

    test patch-remove-psp.yaml

--- bases/tools/Makefile.custom.mk
+++ bases/tools/Makefile.custom.mk
@@ -100,7 +100,7 @@ ifeq ($(FORCE_CRDS),1)
 endif
 ifeq ($(ENFORCE_PSS),1)
 	@# This makes sense only for build-flux-app-cluster, but makes no charm to other targets
-	$(YQ) e -i '(.helmCharts[] | select(.name == "flux-app") | .valuesInline.policyException.enforce) = false' $(TMP_BASE)/kustomization.yaml
+	$(YQ) e -i '.patchesStrategicMerge += ["https://raw.githubusercontent.com/${BASE_REPOSITORY}/${MCB_BRANCH}/extras/flux/patch-remove-psp.yaml"]' $(TMP_BASE)/kustomization.yaml
 	$(YQ) e -i '(.helmCharts[] | select(.name == "flux-app") | .valuesInline.global.podSecurityStandards.enforced) = true' $(TMP_BASE)/kustomization.yaml
 endif
 ifdef BOOTSTRAP_CLUSTER
