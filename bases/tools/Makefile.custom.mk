e753319050aacf0bf7bdfd9f7f44cb8605f13515
Spyros Synodinos <spyros@giantswarm.io>
2024-03-07 12:41:03 +0200

    try patchesStrategicMerge only on build-flux-app-giantswarm

--- bases/tools/Makefile.custom.mk
+++ bases/tools/Makefile.custom.mk
@@ -109,6 +109,9 @@ ifneq ($(filter build-flux-app-customer,$(MAKECMDGOALS)),)
 	$(YQ) e -i '(.helmCharts[] | select(.name == "flux-app") | .valuesInline.policyException.enforce) = false' $(TMP_BASE)/kustomization.yaml
 endif
 endif
+ifneq ($(filter build-flux-app-giantswarm,$(MAKECMDGOALS)),)
+	$(YQ) e -i '.patchesStrategicMerge += ["https://raw.githubusercontent.com/${BASE_REPOSITORY}/${MCB_BRANCH}/extras/flux/patch-remove-psp.yaml"]' $(TMP_BASE)/kustomization.yaml
+endif
 	$(KUSTOMIZE) build --load-restrictor LoadRestrictionsNone --enable-helm --helm-command="$(HELM)" $(TMP_BASE) -o output/flux-app-v${FLUX_MAJOR_VERSION}-$(SUFFIX).yaml
 	rm -rf $(TMP_BASE)
 
