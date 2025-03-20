c2f831c1e8014e741a0e867ebefa13d0908fe0ef
Théo Brigitte <theo.brigitte@gmail.com>
2025-03-20 11:14:38 +0100

    Add silences validation in CI (#190)
    Towards: https://github.com/giantswarm/giantswarm/issues/30437
    
    This PR adds a GitHub action to validate silences files in CI. The PR
    does:
    
    - Add a re-usable Github action
    - Add Github workflow to run the action in this directory
    - Change the `silences-validate.sh` script to work with this repository,
    it was mostly taken from
    https://github.com/giantswarm/silences/blob/main/scripts/validate-files.sh,
    with some modifications:
      - added check for kustomization resources
      - added silences directory path as argument
      - added color output for errors, to ease reading in CI logs
      - changed extension check to ensure .yaml extensions
      - changed yq path to work with this repository
      - fixed returned value on error using pipefail
      - fixed undetected empty valid-until annotation

---
+++ .github/actions/silences-validate/silences-validate.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+YQ="bin/yq"
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+error() {
+  echo -e "${RED}$*${NC}" >&2
+}
+
+# yaml_extension fail if file does not have .yaml extension
+yaml_extension() {
+  if ! [[ "$1" =~ .*\.yaml ]]; then
+    error "  [err] wrong extension, please use .yaml"
+    return 1
+  fi
+  echo "  [ok] .yaml extension"
+}
+
+# valid_until_date fail if valid-until annotation is missing or invalid
+valid_until_date() {
+  valid_until_annotation="$($YQ e '.metadata.annotations.valid-until' "$1")" || return 1
+  if [[ -n "$valid_until_annotation" ]] && [[ "$valid_until_annotation" != "null" ]]; then
+    if ! valid_until="$(date --rfc-3339=date -d "${valid_until_annotation}" 2>&1)"; then
+      error "  [err] valid until : $valid_until"
+      return 1
+    fi
+    echo "  [ok] valid until correct $valid_until"
+  else
+    error "  [err] valid until required"
+    return 1
+  fi
+}
+
+# validate_kustomization_resources fail if resource is not found in kustomization.yaml
+validate_kustomization_resources() {
+  file_basename="$(basename "$1")"
+  if ! echo "${KUSTOMIZATION_RESOURCES}" | grep -qE "^${file_basename}$"; then
+    error "  [err] resource ${file_basename} not found in kustomization.yaml"
+    return 1
+  fi
+  echo "  [ok] resource ${file_basename} found in kustomization.yaml"
+}
+
+main() {
+  echo "> start"
+
+  # Get default branch and merge base to compare against
+  default_branch="$(git remote show origin | grep 'HEAD' | cut -d':' -f2 | tr -d '[[:space:]]')"
+  merge_base="$(git merge-base HEAD "origin/$default_branch")"
+  # Get the root of the git repository
+  git_root="$(git rev-parse --show-toplevel)"
+
+  silences_path="${git_root}/${1}"
+  # Assume kustomization.yaml is at root of silences path
+  kustomization_path="$silences_path/kustomization.yaml"
+
+  # List resources referenced in kustomization.yaml
+  KUSTOMIZATION_RESOURCES="$($YQ e '.resources[]' "$kustomization_path")"
+
+  # Detect files which changed between the current HEAD and the remote default branch in the silences path
+  mapfile -t changed_files < <(git --no-pager diff --name-only "$merge_base" HEAD -- "${silences_path}" ":(exclude)$kustomization_path")
+  echo "> found ${#changed_files[@]} changed files between $(git rev-parse --abbrev-ref HEAD) and $default_branch"
+
+  # Run file checks
+  local has_error=false
+  for file in "${changed_files[@]}"; do
+    file_path="${git_root}/${file}"
+    echo "> checking $file_path"
+
+    yaml_extension "$file_path"              || has_error=true
+    valid_until_date "$file_path"            || has_error=true
+    validate_kustomization_resources "$file" || has_error=true
+
+  done
+
+  if $has_error; then
+    error "> failed"
+    exit 1
+  fi
+
+  echo "> success"
+}
+
+ main "$@"
